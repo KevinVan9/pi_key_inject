c6589037a41edb0eef46f98db99bfdb4c131779f
Kevin Van <kvva@student.unimelb.edu.au>
2023-07-13 20:32:12 +1000

    remove useless string import

--- inject.py
+++ inject.py
@@ -4,13 +4,11 @@
 # https://stackoverflow.com/questions/49887540/improve-python-code
 # Nerd guides on RPi gadget mode as a keyboard
 
-
-import string, ascii
 from codes import codes
 
 NULL_CHAR = chr(0)
 
-# write bytes to enabled gadget mode interface 
+# write bytes to enabled gadget mode interface
 def write_report(report):
     with open('/dev/hidg0', 'rb+') as fd:
         fd.write(report.encode())
@@ -20,6 +18,6 @@ def send_string(s):
     for ch in s:
         write_report(codes[ch])
         write_report(NULL_CHAR*8) #Release key
-    
+
 send_string("username\tpassword\r")
 
